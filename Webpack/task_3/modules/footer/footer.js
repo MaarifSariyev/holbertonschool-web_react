e90ff79ef1ff1baf01bf77abaa8004ed67fe39e9
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-11-24 22:10:35 +0400

    Create footer.js

---
+++ Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,8 @@
+
+// interesting jquery import thanks to babel
+import $ from 'jquery';
+// styles
+import './footer.css';
+
+// append elements
+$('body').append(`<p>Copyright - Holberton School</p>`);
